1f47774642979c8e53deee34cafda2d50477f635
RobertTumele <67093265+RobertTumele@users.noreply.github.com>
2020-10-31 18:00:51 +0200

    Add files via upload

---
+++ TumeleRobert/L05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace L05
+{
+    class Program
+    {
+        private static CloudTableClient tableClient;
+        private static CloudTable studentsTable;
+        static void Main(string[] args)
+        {
+            Task.Run(async()=>{await Initialize();})
+            .GetAwaiter();
+            .GetResult();
+        }
+
+        static async Task Initialize(){
+            string storageConnectionString = "";
+
+            var account = CloudStorageAccount.Parse(storageConnectionString);
+            tableClient = account.CreateTableClient();
+
+            studentsTable = tableClient.GetTableReference("studenti");
+
+            await studentsTable.CreateIfNotExistsAsync();
+
+            await AddNewStudent();
+        }
+    }
+}
